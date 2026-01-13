ea3d7961c831ee327174e31705f622cfc216fb02
bushralaique <bushralaique18@gmail.com>
2026-01-14 00:22:51 +0500

    Add files via upload

---
+++ Q7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main
+{
+public static void main(String[]args)
+{
+ Scanner sc=new Scanner(System.in);
+ System.out.println("ENTER THE NUMBER");
+ int n=sc.nextInt();
+ if(n%2==1)
+ {
+ System.out.println("THE NUMBER YOU ENTERED IS ODD");
+ }
+ else if(n%2==-1)
+ {
+ System.out.println("THE NUMBER YOU ENTERED IS ODD");
+ }
+ else
+ {
+System.out.println("THE NUMBER YOU ENTERED IS EVEN");     
+ }
+}
+}
